45491896d9eb489d4ff4306499a044218cb608a2
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-03 16:08:00 +0700

    Update useQuery hook - add method appendQueryUrl

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,10 +1,23 @@
 import { useMemo } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 
 const useQuery = () => {
   const { search } = useLocation();
+  const history = useHistory();
 
-  return useMemo(() => new URLSearchParams(search), [search]);
+  const query = useMemo(() => new URLSearchParams(search), [search]);
+
+  const appendQueryUrl = (object) => {
+    for (const key in object) {
+      query.set(key, object[key]);
+    }
+
+    history.push({
+      search: `?${query.toString()}`,
+    });
+  };
+
+  return { query, appendQueryUrl };
 };
 
 export default useQuery;
