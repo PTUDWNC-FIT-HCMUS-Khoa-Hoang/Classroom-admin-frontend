7c2f448ff0f9de3b9de6e5843c8ec3ee6af9e347
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-03 16:09:13 +0700

    Update usePagination hook - using appendQueryUrl method & add sortBy, order states

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,28 +1,49 @@
-import { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import useQuery from './useQuery';
 
 const usePagination = () => {
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(10);
-  const history = useHistory();
-  const query = useQuery();
+  const [sortBy, setSortBy] = useState('');
+  const [order, setOrder] = useState('');
+
+  const { query, appendQueryUrl } = useQuery();
 
   const handlePaginationChange = (newPage, newPerPage) => {
-    history.push({
-      search: `?page=${newPage || page}&perPage=${newPerPage || perPage}`,
+    appendQueryUrl({
+      page: newPage,
+      perPage: newPerPage,
     });
   };
 
+  const handleSortChange = (sortBy, order) => {
+    appendQueryUrl({
+      sortBy,
+      order,
+    });
+  };
+
+  // subscription
   useEffect(() => {
     const queryPage = parseInt(query.get('page'));
     const queryPerPage = parseInt(query.get('perPage'));
+    const querySortBy = query.get('sortBy');
+    const queryOrder = query.get('order');
 
     setPage(queryPage || 1);
     setPerPage(queryPerPage || 10);
+    setSortBy(querySortBy || '');
+    setOrder(queryOrder || '');
   }, [query]);
 
-  return { page, perPage, handlePaginationChange };
+  return {
+    page,
+    perPage,
+    sortBy,
+    order,
+    handlePaginationChange,
+    handleSortChange,
+  };
 };
 
 export default usePagination;
