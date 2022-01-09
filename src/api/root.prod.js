73abe04cfd2b7f3c8feaea71f4653a2e4f813b15
khoatrn-dev <khoatrn.dev@gmail.com>
2022-01-09 16:10:04 +0700

    Update product environment server url

--- src/api/root.prod.js
+++ src/api/root.prod.js
@@ -1,7 +1,7 @@
 // Not setup real backend service yet
 const prodEnv = {
-  baseDomain: 'https://khoatrn-classroom-v1.herokuapp.com',
-  basePostUrl: 'https://khoatrn-classroom-v1.herokuapp.com',
+  baseDomain: 'https://advanced-web-classroom-admin.herokuapp.com',
+  basePostUrl: 'https://advanced-web-classroom-admin.herokuapp.com',
 };
 
 export default prodEnv;
